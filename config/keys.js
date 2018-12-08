cf7d9c01345f15dc9549853e67b34733e59d205a
Sean Swan <ogpswan@YAHOO.COM>
2018-12-07 19:28:01 -0800

    added nodemon deps

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports= {
   mongoURI:
-  'mongodb://SeanSwanSavesouls7777777ogpswan@yahoo.com.mlab.com:27664/mean-social'
+  'mongodb://SeanSwanSavesouls7777777.mlab.com:27664/mean-social'
 }
